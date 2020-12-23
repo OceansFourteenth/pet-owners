6db1cbbfb953e842c1607002b86675984dd790ca
Woseley,N. <OceansFourteenth@github.com>
2020-12-23 11:51:53 -0500

    Added plugins to define property descriptions

---
+++ src/main/java/com/oceansfourteenth/petowners/extensions/swagger/ApiModelPropertyDescriptionAnnotationPlugin.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.oceansfourteenth.petowners.extensions.swagger;
+
+import java.util.Optional;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import springfox.bean.validators.plugins.Validators;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.schema.ModelPropertyBuilderPlugin;
+import springfox.documentation.spi.schema.contexts.ModelPropertyContext;
+import springfox.documentation.swagger.common.SwaggerPluginSupport;
+
+/**
+ * @author OceansFourteenth
+ *
+ */
+@Component
+@Order(SwaggerPluginSupport.SWAGGER_PLUGIN_ORDER)
+public class ApiModelPropertyDescriptionAnnotationPlugin implements ModelPropertyBuilderPlugin {
+
+	@Override
+	public boolean supports(DocumentationType delimiter) {
+		return SwaggerPluginSupport.pluginDoesApply(delimiter);
+	}
+
+	@Override
+	public void apply(ModelPropertyContext context) {
+		Optional<ApiModelPropertyDescription> description = Validators.annotationFromBean(context, ApiModelPropertyDescription.class);
+		if (description.isPresent()) {
+			context.getSpecificationBuilder().description(description.get().value());
+		}
+	}
+
+}
