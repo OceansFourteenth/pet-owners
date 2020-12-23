a94c9a44887105d1c81ff31f19181c016badf156
Woseley,N. <OceansFourteenth@github.com>
2020-12-23 01:55:39 -0500

    Updated to OpenApi

--- src/main/java/com/oceansfourteenth/petowners/config/SpringfoxConfig.java
+++ src/main/java/com/oceansfourteenth/petowners/config/SpringfoxConfig.java
@@ -22,7 +22,7 @@ public class SpringfoxConfig {
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
+		return new Docket(DocumentationType.OAS_30)
 				.select()
 				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any())
